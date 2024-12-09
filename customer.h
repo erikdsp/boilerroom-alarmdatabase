c5255548950a66ef02a8190b7f162f90300cd1c8
NewNamesAreHard <oscar.asserlund@gmail.com>
2024-12-09 18:49:46 +0100

    Initial customer functionality(Print/Add/Delete/Tests)

---
+++ customer.h
@@ -0,0 +1,115 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#include "structs.h"
+
+namespace input
+{
+    static int get(int& value, const std::string& prompt)
+    {
+        std::cout << prompt;
+        std::cin >> value;
+        std::cin.ignore();
+        return value;
+    }
+
+    static double get(double& value, const std::string& prompt)
+    {
+        std::cout << prompt;
+        std::cin >> value;
+        std::cin.ignore();
+        return value;
+    }
+
+    static unsigned int get(unsigned int& value, const std::string& prompt)
+    {
+        std::cout << prompt;
+        std::cin >> value;
+        return value;
+    }
+
+    static std::string get(const std::string& prompt)
+    {
+        std::cout << prompt;
+        std::string value;
+        std::getline(std::cin, value);
+        return value;
+    }
+};
+
+namespace customer
+{
+    void customer_print(std::map<uint32_t, Customer>& companies);
+    void customer_create(std::map<uint32_t, Customer>& companies);
+    void test_customer_create(std::map<uint32_t, Customer>& companies, uint32_t runs);
+    void test_fill_customer_create(std::map<uint32_t, Customer>& companies, uint32_t runs);
+
+    void customer_print(std::map<uint32_t, Customer>& companies)
+    {
+        std::cout << "List of companies:\n";
+        for (const auto& [id, customer] : companies)
+        {
+            std::cout << "Company ID: " << id << "\n";
+            std::cout << "Name: " << customer.name << "\n";
+            std::cout << "Company Address: " << customer.address << "\n";
+            std::cout << "Company Users: ";
+            for (const auto& user : customer.users)
+            {
+                std::cout << user << " ";
+            }
+        }
+    }
+
+    void customer_create(std::map<uint32_t, Customer>& companies)
+    {
+        uint32_t i = 0;
+        while (companies.find(i) != companies.end())
+        {
+            ++i;
+        }
+        companies[i].name = input::get("\nEnter customer name: ");
+        companies[i].address = input::get("\nEnter customer address: ");
+        uint32_t users_to_add = input::get(users_to_add, "\nEnter amount of users for this customer: ");
+        companies[i].users.push_back(users_to_add);
+    }
+
+    void customer_delete(std::map<uint32_t, Customer>& companies)
+    {
+        uint32_t id = 0;
+        id = input::get(id, "\nInput ID of customer to be deleted: ");
+        if (companies.find(id) != companies.end())
+        {
+            companies.erase(id);
+            std::cout << "Customer with ID: " << id << " was removed.\n";
+        }
+        else
+        {
+            std::cout << "No customer with the ID: " << id << " was found.\n";
+            return;
+        }
+    }
+
+    void test_fill_customer_create(std::map<uint32_t, Customer>& companies, uint32_t runs)
+    {
+        for (uint32_t i = 0; i < runs; ++i)
+        {
+            companies[i].name = "Test Company " + std::to_string(i);
+            companies[i].address = "Test Lane " + std::to_string(i);
+            companies[i].users = {100 + i};
+        }
+    }
+
+    void test_customer_create(std::map<uint32_t, Customer>& companies, uint32_t runs)
+    {
+        test_fill_customer_create(companies, runs);
+        customer_print(companies);
+    }
+};
+
+#endif
