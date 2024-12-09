a5d2249ee69c88d8241d032ff162428d2081c1cf
Johan Modin <carljohan.modin@gmail.com>
2024-12-09 10:57:13 +0100

    Add validation for pin and random rfid

--- User.h
+++ User.h
@@ -3,24 +3,38 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <cstdlib>
 
 class User {
 public:
     std::string pin;
-    std::string rfid;
+    int rfid;
     std::string passphrase;
     static User create_user();
 };
  
 User User::create_user(){
 
+    std::srand(time(NULL));
     User temp_user;
+
+    // Randomizes an rfid (starting from 1000000), room for improvement here.
+    temp_user.rfid = 1000000 + std::rand() % (10000000);
     
-    std::cout << "\nPlease enter desired pin:";
-    std::getline(std::cin, temp_user.pin);
-    std::cout << "\nPlease enter desired rfid:";
-    std::getline(std::cin, temp_user.rfid);
-    std::cout << "\nPlease enter desired passphrase:";
+    // Loop until correct size (4) of type string is entered.
+    do{
+        std::cout << "\nPlease enter desired pin: ";
+        std::getline(std::cin,temp_user.pin);
+            if(temp_user.pin.size() != 4)
+            {
+                std::cout << "Invalid PIN\nExpected format: [XXXX]";
+            }
+            else
+                std::cout << "-Valid PIN-";
+    } while(temp_user.pin.size() != 4);
+
+    std::cout << "\nRFID: " << temp_user.rfid; 
+    std::cout << "\nPlease enter desired passphrase: ";
     std::getline(std::cin, temp_user.passphrase);
 
     return temp_user;
