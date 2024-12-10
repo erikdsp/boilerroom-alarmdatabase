361f23d82522f140be929b0309ff76cf82bebe76
Erik Dahl <erik@iunderlandet.se>
2024-12-10 11:38:21 +0100

    update User.h from user branch

--- User.h
+++ User.h
@@ -1,11 +1,10 @@
-#ifndef DATABASE_USER_H
-#define DATABASE_USER_H
-
 #include <fstream>
 #include <cctype>
 #include <iostream>
 #include <map>
 #include <vector>
+#include <cstdlib>
+#include <numeric>
 
 class User {
 public:
@@ -15,20 +14,31 @@ public:
     static User create_user();
 };
  
-/*  
 User User::create_user(){
 
+    std::srand(time(NULL));
     User temp_user;
+    int temp_random_number{};
+
+    // Randomizes an rfid (starting from 1000000), room for improvement here.
+    temp_random_number = 100000000000 + std::rand() % (RAND_MAX);
+    temp_user.rfid = std::to_string(temp_random_number); 
+        
+    // Loop until correct size (4) of type string is entered.
+    do{
+        std::cout << "\nPlease enter desired pin: ";
+        std::getline(std::cin, temp_user.pin);
+            if(temp_user.pin.size() != 4)
+            {
+                std::cout << "Invalid PIN\nExpected format: [XXXX]";
+            }
+            else
+                std::cout << "-Valid PIN-";
+    } while(temp_user.pin.size() != 4);
     
-    std::cout << "\nPlease enter desired pin:";
-    std::getline(std::cin, temp_user.pin);
-    std::cout << "\nPlease enter desired rfid:";
-    std::getline(std::cin, temp_user.rfid);
-    std::cout << "\nPlease enter desired passphrase:";
+    std::cout << "\nRFID: " << temp_user.rfid; 
+    std::cout << "\nEnter desired passphrase: ";
     std::getline(std::cin, temp_user.passphrase);
 
     return temp_user;
 }
- */
-
-#endif
